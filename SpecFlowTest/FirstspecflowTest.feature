1beeb4fa144c93b288b667b1f74fdf126e8e19c9
Navya Hegde <navyahegdesh@gmail.com>
2021-05-31 11:09:06 +0530

    Code changes

---
+++ SpecFlowTest/FirstspecflowTest.feature
@@ -0,0 +1,57 @@
+﻿Feature: FirstspecflowTest
+	Simple calculator for adding two numbers
+
+@mytag
+Scenario Outline: Add two numbers
+	Given the first number is 50
+	And the second number is 70
+	When the two numbers are added
+	Then the result should be 120
+
+@mytag @Integration @Regression
+Scenario Outline: Addion of two numbers 
+	Given the first number is <input1>
+	And the second number is <input2>
+	When the two numbers are added
+	Then the result should be <sum>
+
+Examples: 
+| input1 | input2 | sum |
+| 50     | 70     | 120 |
+| 10     | 10     | 20  |
+
+@mytag @Integration @Regression
+Scenario Outline: subtraction of two numbers 
+	Given the first number is <input1>
+	And the second number is <input2>
+	When the two numbers are subtraction
+	Then the result should be <sub>
+
+Examples: 
+| input1 | input2 | sub |
+| 70     | 50     | 20 |
+| 10     | 10     | 0  |
+
+@multiplication
+Scenario Outline: multiplication of two numbers 
+	Given the first number is <input1>
+	And the second number is <input2>
+	When the two numbers are multiplied
+	Then the result should be <mul>
+
+Examples: 
+| input1 | input2 | mul |
+| 10     | 20     | 200 |
+| 20     | 5      | 100 |
+
+@Division
+Scenario Outline: Division of two numbers 
+	Given the first number is <input1>
+	And the second number is <input2>
+	When the two numbers are divided
+	Then the result should be <div>
+
+Examples: 
+| input1 | input2 | div | 
+| 100    | 20     | 5	|
+| 20     | 5      | 4   |
---
+++ SpecFlowTest/FirstspecflowTestDefination.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TechTalk.SpecFlow;
+using Xunit;
+
+namespace SpecFlowTest
+{
+    [Binding]
+    public sealed class FirstspecflowTestDefination
+    {
+        // For additional details on SpecFlow step definitions see https://go.specflow.org/doc-stepdef
+
+        private readonly ScenarioContext _scenarioContext;
+        int x, y, sum = 0, sub = 0;
+        public FirstspecflowTestDefination(ScenarioContext scenarioContext)
+        {
+            _scenarioContext = scenarioContext;
+        }
+        [Given(@"the first number is (.*)")]
+        public void GivenTheFirstNumberIs(int p0)
+        {
+            x = p0;
+        }
+
+        [Given(@"the second number is (.*)")]
+        public void GivenTheSecondNumberIs(int p0)
+        {
+            y = p0;
+        }
+
+        [When(@"the two numbers are added")]
+        public void WhenTheTwoNumbersAreAdded()
+        {
+            sum = x + y;
+        }
+
+        [Then(@"the result should be (.*)")]
+        public void ThenTheResultShouldBe(int p0)
+        {
+            Assert.Equal(p0, sum);
+            //Assert.Equal(p0, sub);
+        }
+
+        [When(@"the two numbers are subtraction")]
+        public void WhenTheTwoNumbersAreSubtraction()
+        {
+            sum = x - y;
+        }
+
+        [When(@"the two numbers are multiplied")]
+        public void WhenTheTwoNumbersAreMultiplied()
+        {
+            sum = x * y;
+        }
+
+        [When(@"the two numbers are divided")]
+        public void WhenTheTwoNumbersAreDivided()
+        {
+            sum = x / y;
+        }        
+    }
+}
